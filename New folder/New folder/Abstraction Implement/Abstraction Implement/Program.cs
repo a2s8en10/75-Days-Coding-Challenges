32156798320af53738b88908995d14ad3faf68f9
Anurag Sahu <anu.sahu9589@gmail.com>
2025-05-27 17:59:15 +0530

    Update Code

---
+++ New folder/New folder/Abstraction Implement/Abstraction Implement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction_Implement
+{
+    abstract class Person
+    {
+        public abstract void Detail();
+        public void Show()
+        {
+            Console.WriteLine("Show Detail....");
+        }
+    }
+    class Student : Person
+    {
+        public override void Detail()
+        {
+            Console.WriteLine("Student is here..");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person student = new Student();
+            student.Detail();
+            student.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
