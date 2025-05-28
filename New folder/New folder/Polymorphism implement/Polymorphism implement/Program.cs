ebb1ce98a0976295912c68adf79d2a36b80abc40
Anurag Sahu <anu.sahu9589@gmail.com>
2025-05-28 18:32:56 +0530

    Update Code

---
+++ New folder/New folder/Polymorphism implement/Polymorphism implement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_implement
+{
+    public class poly
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            poly obj = new poly();
+            int num = obj.Add(12, 38);
+            string str = obj.Add("Anurag", "Sahu");
+            Console.WriteLine(num + " - " + str);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ New folder/New folder/Polymorphism implement/Polymorphism implement/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Polymorphism implement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Polymorphism implement")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9259f3fe-9d9d-4480-8b17-d5f58792c5c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
