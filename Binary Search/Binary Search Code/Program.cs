50db3df6041ee00063b3d4ad6628c54de2597e0c
Anurag Sahu <anu.sahu9589@gmail.com>
2025-10-05 22:13:29 +0530

    Binary Search

---
+++ Binary Search/Binary Search Code/Program.cs
@@ -0,0 +1,35 @@
+﻿// int[] num = new int[5] { 2, 5, 9, 11, 34 };
+int[] num = { 2, 5, 9, 11, 34 };
+int n = num.Length;
+int low = 0, high = n - 1;
+int target = 35;
+bool isTarget = false;
+int mid = 0;
+
+while (low <= high)
+{
+    mid = (low + high) / 2;
+    if (num[mid] == target)
+    {
+        isTarget = true;
+        break;
+    }
+    if (num[mid] < target)
+    {
+        low = mid + 1;
+    }
+    else
+    {
+        high = mid - 1;
+    }
+}
+if (isTarget)
+{
+    Console.WriteLine($"{target} index of - {mid}");
+}
+else
+{
+    System.Console.WriteLine("Not Fount !!");
+}
+
+Console.ReadLine();
---
+++ Binary Search/count-negative-numbers-in-a-sorted-matrix/Program.cs
@@ -0,0 +1,19 @@
+﻿int[][] num = [[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]];
+
+int count = 0;
+int i, j;
+
+int n = num.Length;
+int m = num[0].Length;
+
+for (i = 0; i < n; i++)
+{
+    for (j = 0; j < m; j++)
+    {
+        if (0 > num[i][j])
+        {
+            count++;
+        }
+    }
+}
+System.Console.WriteLine("Negative number is : " + count);
